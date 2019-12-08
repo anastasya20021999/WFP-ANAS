d31dbf0343eb8ae382e022aefcaa0c083badebb0
Griselda Jessica <55136410+griseldajessica@users.noreply.github.com>
2019-12-08 16:04:37 +0700

    master pindah class index.php

--- resources/views/master/create.blade.php
+++ resources/views/master/create.blade.php
@@ -134,8 +134,8 @@
     <tr>
       <td><font face="Monospace" color="white" ><strong style="font-size: 17px;">Jenis &nbsp;&nbsp;</strong></font></td>
       <td><select name="jenis_master"> 
-        <option value="pemasukkan">Pemasukkan</option>
-        <option value="pengeluaran">Pengeluaran</option>
+        <option value="Pemasukkan">Pemasukkan</option>
+        <option value="Pengeluaran">Pengeluaran</option>
       </select></td>
      <!--  <td><input type="text" name="jenis_master"></td> -->
     </tr>
@@ -151,135 +151,6 @@
             </div>
         </div>
     </div>
-    <!-- slider_area_end -->
-
-    <!-- about_area_start -->
-    
-    <!-- about_area_end -->
-
-    <!-- popular_courses_start -->
-   
-    <!-- popular_courses_end-->
-
-
-    <!-- testimonial_area_start -->
-    
-    <!-- testimonial_area_end -->
-
-    <!-- our_courses_start -->
-    
-    <!-- subscribe_newsletter_end -->
-
-    <!-- our_latest_blog_start -->
-    
-    <!-- our_latest_blog_end -->
-
-
-    <!-- footer -->
-    <footer class="footer footer_bg_1">
-        <div class="footer_top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-4 col-md-6 col-lg-4">
-                        <div class="footer_widget">
-                            <div class="footer_logo">
-                                <a href="#">
-                                    <img src="{{asset('bootstrap/img/logo1.png')}}" alt="">
-                                </a>
-                            </div>
-                            <p>
-                                Firmament morning sixth subdue darkness creeping gathered divide our let god moving.
-                                Moving in fourth air night bring upon it beast let you dominion likeness open place day
-                                great.
-                            </p>
-                            <div class="socail_links">
-                                <ul>
-                                    <li>
-                                        <a href="#">
-                                            <i class="ti-facebook"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="ti-twitter-alt"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-instagram"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-youtube-play"></i>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="col-xl-2 offset-xl-1 col-md-6 col-lg-3">
-                        <div class="footer_widget">
-                            <h3 class="footer_title">
-                                Courses
-                            </h3>
-                            <ul>
-                                <li><a href="#">Wordpress</a></li>
-                                <li><a href="#"> Photoshop</a></li>
-                                <li><a href="#">Illustrator</a></li>
-                                <li><a href="#">Adobe XD</a></li>
-                                <li><a href="#">UI/UX</a></li>
-                            </ul>
-
-                        </div>
-                    </div>
-                    <div class="col-xl-2 col-md-6 col-lg-2">
-                        <div class="footer_widget">
-                            <h3 class="footer_title">
-                                Resourches
-                            </h3>
-                            <ul>
-                                <li><a href="#">Free Adobe XD</a></li>
-                                <li><a href="#">Tutorials</a></li>
-                                <li><a href="#">About</a></li>
-                                <li><a href="#"> About</a></li>
-                                <li><a href="#"> Contact</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-md-6 col-lg-3">
-                        <div class="footer_widget">
-                            <h3 class="footer_title">
-                                Address
-                            </h3>
-                            <p>
-                                200, D-block, Green lane USA <br>
-                                +10 367 467 8934 <br>
-                                edumark@contact.com
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="copy-right_text">
-            <div class="container">
-                <div class="footer_border"></div>
-                <div class="row">
-                    <div class="col-xl-12">
-                        <p class="copy_right text-center">
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- footer -->
-
 
     <!-- form itself end-->
     <form id="test-form" class="white-popup-block mfp-hide">
