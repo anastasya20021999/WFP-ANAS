6146898a9df702893be5b5e9ba1d5552d4777d2a
Griselda Jessica <55136410+griseldajessica@users.noreply.github.com>
2019-12-08 16:09:12 +0700

    ubah index plus punya nolan

--- resources/views/submaster/tambah.blade.php
+++ resources/views/submaster/tambah.blade.php
@@ -49,12 +49,17 @@
                             <div class="main-menu  d-none d-lg-block">
                                 <nav>
                                     <ul id="navigation">
-                                        <li><a class="active" href="{{url('/')}}">home</a></li>
-                                        @if(Auth::user()) 
-                                        <li><a href="{{ url('saldos') }}">Saldo</a></li>
+                                        <li><a href="{{url('/')}}">home</a></li></li>
+                                        @if (Auth::user()) 
+                                            <li><a href="{{ url('saldos') }}" class="active">Saldo</a></li>
                                         <li><a href="{{ url('masters') }}">Master</a></li>
                                         <li><a href="{{ url('transaksis') }}">Transaksi</a></li>
-                                        <li><a href="{{ url('tabungans') }}">Tabungan</a></li>
+                                        <li><a href="#">blog <i class="ti-angle-down"></i></a>
+                                            <ul class="submenu">
+                                                <li><a href="{{asset('bootstrap/blog.html')}}">blog</a></li>
+                                                <li><a href="{{asset('bootstrap/single-blog.html')}}">single-blog</a></li>
+                                            </ul>
+                                        </li>
                                         <li><a href="{{ url('laporan') }}">Laporan</a></li>
                                         @endif
                                     </ul>
@@ -72,7 +77,7 @@
                                 </a>
                                 @else
                                 <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" style="color: white;">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"style="color: white;">
                                     {{ Auth::user()->username }} <span class="caret"></span>
                                 </a>
 
@@ -115,160 +120,44 @@
         <div class="single_slider d-flex align-items-center justify-content-center slider_bg_1">
             <div class="container">
                 <div class="row align-items-center justify-content-center">
-                    <div class="col-xl-6 col-md-6">
-                        <div class="illastrator_png">
-                            <img src="{{asset('bootstrap/img/banner/logohome.png')}}" alt="">
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-md-6">
-                        <div class="slider_info">
-                            <h3>
-                                Manage Your <br>
-                                Own Money <br>
-                                With Cuan Lover
-                                <br>
-                                @if(Auth::user())
-                                
-                                @endif
-                                </h3>
-                                
-                            <a href="#" class="boxed_btn">Browse Our Courses</a>
-                        </div>
-                    </div>
+                  <h1>Tambah SubMaster</h1>
+<div class="container">
+  <center>
+<form method="post" action="{{url('submasters/tambah/proses')}}">
+  <table>
+    <input type="hidden" name="_token" value="{{csrf_token()}}"/>
+    <input type="hidden" name="user" value="{{Auth::user()->id}}"/>
+    <tr>
+        <td><font face="Monospace" color="white"><strong style="font-size: 17px;">Master &nbsp;&nbsp;</strong></font></td> 
+        <td>
+        <select name="select_master"> 
+        @foreach($duar as $no => $d)
+        <option value="{{$d->id}}">{{$d->id}} - {{$d->nama}}</option>
+        @endforeach
+        </select>
+        </td>
+    </tr>
+    <tr>
+      <td><font face="Monospace" color="white"><strong style="font-size: 17px;">Nama &nbsp;&nbsp;</strong></font></td> 
+      <td><input type="text" name="nama_submaster" ></td>
+    </tr>
+    <tr>
+      <td><font face="Monospace" color="white" ><strong style="font-size: 17px;">Jenis Pembayaran &nbsp;&nbsp;</strong></font></td>
+      <td><input type="text" name="jenis_pembayaran"></td>
+    </tr>
+    
+  </table>
+  <input type="submit" style="font-family: monospace;" name="Simpan" value="Simpan"/>
+</form>
+</center>
+</div>
+</td>
+          
                 </div>
             </div>
         </div>
     </div>
-    <!-- slider_area_end -->
-
-    <!-- about_area_start -->
     
-    <!-- about_area_end -->
-
-    <!-- popular_courses_start -->
-   
-    <!-- popular_courses_end-->
-
-
-    <!-- testimonial_area_start -->
-    
-    <!-- testimonial_area_end -->
-
-    <!-- our_courses_start -->
-    
-    <!-- subscribe_newsletter_end -->
-
-    <!-- our_latest_blog_start -->
-    
-    <!-- our_latest_blog_end -->
-
-
-    <!-- footer -->
-    <footer class="footer footer_bg_1">
-        <div class="footer_top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-4 col-md-6 col-lg-4">
-                        <div class="footer_widget">
-                            <div class="footer_logo">
-                                <a href="#">
-                                    <img src="{{asset('bootstrap/img/logo1.png')}}" alt="">
-                                </a>
-                            </div>
-                            <p>
-                                Firmament morning sixth subdue darkness creeping gathered divide our let god moving.
-                                Moving in fourth air night bring upon it beast let you dominion likeness open place day
-                                great.
-                            </p>
-                            <div class="socail_links">
-                                <ul>
-                                    <li>
-                                        <a href="#">
-                                            <i class="ti-facebook"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="ti-twitter-alt"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-instagram"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-youtube-play"></i>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="col-xl-2 offset-xl-1 col-md-6 col-lg-3">
-                        <div class="footer_widget">
-                            <h3 class="footer_title">
-                                Courses
-                            </h3>
-                            <ul>
-                                <li><a href="#">Wordpress</a></li>
-                                <li><a href="#"> Photoshop</a></li>
-                                <li><a href="#">Illustrator</a></li>
-                                <li><a href="#">Adobe XD</a></li>
-                                <li><a href="#">UI/UX</a></li>
-                            </ul>
-
-                        </div>
-                    </div>
-                    <div class="col-xl-2 col-md-6 col-lg-2">
-                        <div class="footer_widget">
-                            <h3 class="footer_title">
-                                Resourches
-                            </h3>
-                            <ul>
-                                <li><a href="#">Free Adobe XD</a></li>
-                                <li><a href="#">Tutorials</a></li>
-                                <li><a href="#">About</a></li>
-                                <li><a href="#"> About</a></li>
-                                <li><a href="#"> Contact</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-md-6 col-lg-3">
-                        <div class="footer_widget">
-                            <h3 class="footer_title">
-                                Address
-                            </h3>
-                            <p>
-                                200, D-block, Green lane USA <br>
-                                +10 367 467 8934 <br>
-                                edumark@contact.com
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="copy-right_text">
-            <div class="container">
-                <div class="footer_border"></div>
-                <div class="row">
-                    <div class="col-xl-12">
-                        <p class="copy_right text-center">
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- footer -->
-
-
     <!-- form itself end-->
     <form id="test-form" class="white-popup-block mfp-hide">
         <div class="popup_box ">
@@ -362,3 +251,5 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 </body>
 
 </html>
+
+
